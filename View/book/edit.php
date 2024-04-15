682fe6ddbffef4aad1a13d7caf8d1b3cc451c7a3
Malek04 <123811220+Malek04@users.noreply.github.com>
2024-04-16 00:24:23 +0100

    update book

--- View/book/edit.php
+++ View/book/edit.php
@@ -10,7 +10,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inserting books</title>
+    <title>Updating Books</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
@@ -21,33 +21,23 @@
             foreach($b as $book){
         ?>
             <div class="mb-3">
-                <label for="id" class="form-label">ID</label>
+                <label for="id_book" class="form-label">ID</label>
                 <input type="text" class="form-control" name="id_book" id="id_book" readonly value="<?=$book->get_id_book()?>">
             </div>
             <div class="mb-3">
-                <label for="genre" class="form-label">Genre</label>
-                </br>
+                <label for="id_genre" class="form-label">Genre</label><br>
                 <select name="id_genre" class="form-select" aria-label="Default select example">
-                <?php
-                    foreach($genres as $genre){
-                        ?>
-                        <option value="<?=$book->get_id_genre()?>"><?=$genre->get_genre()?></option>
-                        <?php
-                    }
-                ?>
+                <?php foreach($genres as $genre): ?>
+                    <option value="<?=$genre->get_id_genre()?>" <?php if($genre->get_id_genre() == $book->get_id_genre()) echo 'selected'; ?>><?=$genre->get_genre()?></option>
+                <?php endforeach; ?>
                 </select>
             </div>
             <div class="mb-3">
-                <label for="id_author" class="form-label">Author</label>
-                </br>
+                <label for="id_author" class="form-label">Author</label><br>
                 <select name="id_author" class="form-select" aria-label="Default select example">
-                <?php
-                    foreach($authors as $author){
-                        ?>
-                        <option value="<?=$author->get_id_author()?>"><?=$author->get_name()?></option>
-                        <?php
-                    }
-                ?>
+                <?php foreach($authors as $author): ?>
+                    <option value="<?=$author->get_id_author()?>" <?php if($author->get_id_author() == $book->get_id_author()) echo 'selected'; ?>><?=$author->get_name()?></option>
+                <?php endforeach; ?>
                 </select>
             </div>
             <div class="mb-3">
@@ -55,7 +45,7 @@
                 <input type="text" class="form-control" name="title" id="title" value="<?=$book->get_title()?>">
             </div>
             <div class="mb-3">
-                <label for="length" class="form-label">length</label>
+                <label for="length" class="form-label">Length</label>
                 <input type="number" class="form-control" name="length" id="length" value="<?=$book->get_length()?>">
             </div>
             <div class="mb-3">
@@ -72,21 +62,21 @@
             </div>
             <div class="mb-3">
                 <label for="image" class="form-label">Image</label>
-                <input type="file" class="form-control" name="image" id="image" value="<?=$book->get_image()?>">
+                <input type="file" class="form-control" name="image" id="image">
             </div>
             <div class="mb-3">
-                <label for="description" class="form-label">Dscription</label>
-                <textarea class="form-control" name="description" id="description" rows="5" cols="173"><?=$book->get_description()?>"</textarea>
+                <label for="description" class="form-label">Description</label>
+                <textarea class="form-control" name="description" id="description" rows="5" cols="173"><?=$book->get_description()?></textarea>
             </div>
             <div class="mb-3">
                 <label for="qte" class="form-label">Quantité</label>
                 <input type="number" class="form-control" name="qte" id="qte" value="<?=$book->get_qte()?>">
             </div>
             <button type="submit" class="btn btn-primary">Modify</button>
-            </form>
         <?php
             }
         ?>
+        </form>
     </div>
 </body>
 </html>
